e70e47b8e66e5f8cb75747240fd4e823bb2c5d74
Nick <nikitaavdeev2000@gmail.com>
2020-07-27 14:37:37 +0500

    Связал три страницы

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('how-it-works/', views.how_it_works, name='how-it-works'),
+    path('testing/', views.testing, name='testing'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
