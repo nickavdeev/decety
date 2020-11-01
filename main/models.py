ffbd0acf140bb2cb01ef63d39f4bb12ea81ba577
Nick <nikitaavdeev2000@gmail.com>
2020-11-02 01:35:56 +0500

    Добавил бота и фотографии

--- main/models.py
+++ main/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 
+class Feedback(models.Model):
+    name = models.CharField('Имя', max_length=100, default='')
+    phone = models.CharField('Телефон', max_length=100, default='')
+    email = models.CharField('Email', max_length=100, default='')
+    message = models.CharField('Сообщение', max_length=800, default='')
+
+    class Meta:
+        verbose_name_plural = "Сообщение"
+        verbose_name = "Сообщения"
+
+    def __str__(self):
+        return f'{self.message}'
+
+
 class Request(models.Model):
     waist = models.CharField("Талия", max_length=100)
     hips = models.CharField("Бёдра", max_length=100)
